a52e05f6a26fd8f27c6baef323c434322b264f25
shun <1939477812@qq.com>
2020-05-26 00:24:16 +0800

    登录UI界面搭建

--- demo2/src/views/login/login.vue
+++ demo2/src/views/login/login.vue
@@ -1,12 +1,166 @@
 <template>
-  <div id="login">hello</div>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          :class="{ current: item.current }"
+          v-for="item in menuTab"
+          :key="item.id"
+          @click="toggleMenu(item)"
+        >{{ item.txt }}</li>
+      </ul>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+        <label>邮箱</label>
+        <el-form-item prop="pass">
+          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+        </el-form-item>
+        <label>密码</label>
+        <el-form-item prop="checkPass">
+          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+        </el-form-item>
+        <label>验证码</label>
+        <el-row :gutter="6">
+          <el-col :span="15">
+            <div class="grid-content bg-purple">
+              <el-form-item prop="age">
+                <el-input v-model.number="ruleForm.age"></el-input>
+              </el-form-item>
+            </div>
+          </el-col>
+          <el-col :span="9">
+            <el-button type="danger" class="block">获取验证码</el-button>
+          </el-col>
+        </el-row>
+        <el-form-item>
+          <el-button type="success" @click="submitForm('ruleForm')" class="btnSubmit">提交</el-button>
+          <!-- <el-button @click="resetForm('ruleForm')">重置</el-button> -->
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ],
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        age: ""
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    toggleMenu(data) {
+      console.log(data);
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      data.current = true;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.block {
+  display: block;
+  width: 100%;
+}
 #login {
   height: 100vh;
-  background-color: #ccc;
+  background-color: #344a5f;
+}
+.login-wrap {
+  position: absolute;
+  width: 330px;
+  margin: 0 auto;
+  left: 50%;
+  top: 40%;
+  transform: translate(-50%, -50%);
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    color: #fff;
+    cursor: pointer;
+  }
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.demo-ruleForm {
+  label {
+    display: block;
+    color: #fff;
+    margin-bottom: 14px;
+  }
+  .btnSubmit {
+    width: 100%;
+  }
 }
 </style>
